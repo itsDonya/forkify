68dbba77b977b1382661dd3b7880e3d048b01659
Donya <thisis2nya@gmail.com>
2023-08-01 23:02:41 +0330

    [UI] separate recipe item & sidebar component

--- components/sidebar/AppSidebar.vue
+++ components/sidebar/AppSidebar.vue
@@ -2,30 +2,18 @@
   <div
     class="w-96 h-full bg-white max-h-full overflow-y-auto overflow-x-hidden"
   >
+    <!-- show error if server returned an error  -->
     <app-error v-if="$store.state.recipesError.length">{{
       $store.state.recipesError
     }}</app-error>
-    <nuxt-link
+
+    <!-- single recipe -->
+    <recipe-item
       :to="'/' + recipe.recipe_id"
       v-for="recipe in recipes"
       :key="recipe.id"
-    >
-      <div
-        class="w-full p-4 hover:bg-gray-200/20 transition-all flex items-center justify-start gap-3 hover:-translate-y-0.5 duration-300"
-      >
-        <img
-          :src="recipe.image_url"
-          class="w-12 h-12 rounded-full"
-          alt="fatoush salad"
-        />
-        <div class="flex flex-col items-start justify-center">
-          <p class="uppercase text-sm text-peach">{{ recipe.title }}</p>
-          <p class="uppercase text-[10px] text-gray-400">
-            {{ recipe.publisher }}
-          </p>
-        </div>
-      </div>
-    </nuxt-link>
+      :recipe="recipe"
+    ></recipe-item>
   </div>
 </template>
 
---
+++ components/sidebar/RecipeItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <nuxt-link :to="'/' + recipe.recipe_id">
+    <div
+      class="w-full p-4 hover:bg-gray-200/20 transition-all flex items-center justify-start gap-3 hover:-translate-y-0.5 duration-300"
+    >
+      <img
+        :src="recipe.image_url"
+        class="w-12 h-12 rounded-full"
+        alt="fatoush salad"
+      />
+      <div class="flex flex-col items-start justify-center">
+        <p class="uppercase text-sm text-peach">{{ recipe.title }}</p>
+        <p class="uppercase text-[10px] text-gray-400">
+          {{ recipe.publisher }}
+        </p>
+      </div>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: "RecipeItem",
+};
+</script>
+
+<script setup>
+// props
+const props = defineProps({
+  recipe: {
+    type: Object,
+    default: function () {
+      return {};
+    },
+  },
+});
+</script>
