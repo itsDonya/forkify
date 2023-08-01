67a5e998a79cabb1cc3423f9316e7be8c5528a75
Donya <thisis2nya@gmail.com>
2023-08-01 19:24:00 +0330

    [UI] create sidebar including recipes coming from api

--- components/AppContainer.vue
+++ components/AppContainer.vue
@@ -3,5 +3,9 @@
     class="w-10/12 h-[86vh] flex flex-col bg-zinc-100 rounded-xl shadow-xl overflow-hidden"
   >
     <app-header></app-header>
+
+    <div class="w-full h-full flex items-start">
+      <app-sidebar></app-sidebar>
+    </div>
   </div>
 </template>
---
+++ components/sidebar/AppSidebar.vue
@@ -0,0 +1,98 @@
+<template>
+  <div
+    class="w-64 h-full bg-white max-h-full overflow-y-auto overflow-x-hidden"
+  >
+    <nuxt-link to="/" v-for="(recipe, i) in recipes" :key="i">
+      <div
+        class="w-full p-4 hover:bg-gray-200/20 transition-all flex items-center justify-start gap-3"
+      >
+        <img :src="recipe.img" class="w-10" alt="fatoush salad" />
+        <div class="flex flex-col items-start justify-center">
+          <p class="uppercase text-sm text-peach">{{ recipe.title }}</p>
+          <p class="uppercase text-[10px] text-gray-400">
+            {{ recipe.description }}
+          </p>
+        </div>
+      </div>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppSidebar",
+};
+</script>
+
+<script setup>
+import { ref } from "@nuxtjs/composition-api";
+
+// variables
+const recipes = ref([
+  {
+    title: "FATOUSH SALAD",
+    description: "WHAT'S GABY COOKINGs",
+    img: "/favicon.png",
+  },
+  {
+    title: "FATOUSH SALAD",
+    description: "WHAT'S GABY COOKINGs",
+    img: "/favicon.png",
+  },
+  {
+    title: "FATOUSH SALAD",
+    description: "WHAT'S GABY COOKINGs",
+    img: "/favicon.png",
+  },
+  {
+    title: "FATOUSH SALAD",
+    description: "WHAT'S GABY COOKINGs",
+    img: "/favicon.png",
+  },
+  {
+    title: "FATOUSH SALAD",
+    description: "WHAT'S GABY COOKINGs",
+    img: "/favicon.png",
+  },
+  {
+    title: "FATOUSH SALAD",
+    description: "WHAT'S GABY COOKINGs",
+    img: "/favicon.png",
+  },
+  {
+    title: "FATOUSH SALAD",
+    description: "WHAT'S GABY COOKINGs",
+    img: "/favicon.png",
+  },
+  {
+    title: "FATOUSH SALAD",
+    description: "WHAT'S GABY COOKINGs",
+    img: "/favicon.png",
+  },
+  {
+    title: "FATOUSH SALAD",
+    description: "WHAT'S GABY COOKINGs",
+    img: "/favicon.png",
+  },
+  {
+    title: "FATOUSH SALAD",
+    description: "WHAT'S GABY COOKINGs",
+    img: "/favicon.png",
+  },
+  {
+    title: "FATOUSH SALAD",
+    description: "WHAT'S GABY COOKINGs",
+    img: "/favicon.png",
+  },
+  {
+    title: "FATOUSH SALAD",
+    description: "WHAT'S GABY COOKINGs",
+    img: "/favicon.png",
+  },
+  {
+    title: "FATOUSH SALAD",
+    description: "WHAT'S GABY COOKINGs",
+    img: "/favicon.png",
+  },
+]);
+</script>
