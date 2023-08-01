a0438048acb7e5aa6b15f0d6cead34da8356dbb5
Donya <thisis2nya@gmail.com>
2023-08-01 20:42:59 +0330

    [feat] get recipes by searchbox value

--- components/header/SearchInput.vue
+++ components/header/SearchInput.vue
@@ -1,6 +1,7 @@
 <template>
   <input
     type="text"
+    v-model="recipe"
     class="w-72 h-full px-4 bg-transparent"
     placeholder="Search over 1,000,000 recipes..."
   />
@@ -10,4 +11,22 @@
 export default {
   name: "SearchInput",
 };
+</script>
+
+<script setup>
+import { computed, useStore } from "@nuxtjs/composition-api";
+
+// variables
+const store = useStore();
+
+// computed
+const recipe = computed({
+  get() {
+    return store.state.recipeTitle;
+  },
+  set(value) {
+    // update recipe title (in store) on every change
+    store.dispatch("updateRecipeTitle", value);
+  },
+});
 </script>
--- store/index.js
+++ store/index.js
@@ -1,21 +1,30 @@
 export const state = () => ({
   recipes: [],
+  recipeTitle: "",
 });
 
 export const mutations = {
   setRecipes(state, recipes) {
     state.recipes = recipes;
   },
+  updateRecipeTitle(state, value) {
+    state.recipeTitle = value;
+  },
 };
 
 export const actions = {
-  async getRecipes({ commit }) {
+  async getRecipes({ state, commit }) {
     await this.$axios
-      .$get("https://forkify-api.herokuapp.com/api/search?q=pizza")
+      .$get(
+        `https://forkify-api.herokuapp.com/api/search?q=${state.recipeTitle}`
+      )
       .then((res) => commit("setRecipes", res.recipes))
       .catch((error) => {
         throw error;
       });
+  },
+  updateRecipeTitle({ commit }, value) {
+    commit("updateRecipeTitle", value);
   },
 };
 
