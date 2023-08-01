579a5bd78a9606652853f0a5d34795ea42273220
Donya <thisis2nya@gmail.com>
2023-08-01 12:57:00 +0330

    [UI] create base icon wrapper

---
+++ components/UI/BaseIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <component :is="icon"></component>
+</template>
+
+<script>
+export default {
+  name: "BaseIcon",
+};
+</script>
+
+<script setup>
+import { computed } from "@nuxtjs/composition-api";
+
+// props
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+    default: "",
+  },
+});
+
+// computed
+const icon = computed(() => {
+  return require(`@/assets/svg/${props.name}.svg?inline`);
+});
+</script>
