3513ef43a003c70647a73bb2130ab4524daba8b8
Donya <thisis2nya@gmail.com>
2023-08-01 10:11:04 +0330

    [core] website configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,29 @@
-
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: "Forkify | Search over 1,000,000 recipes",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-    "@/assets/css/main.css"
-  ],
+   ** Global CSS
+   */
+  css: ["@/assets/css/main.css"],
   /*
    ** Auto-Import & Register Components
    */ components: [
@@ -34,29 +33,24 @@ export default {
     },
   ],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    "@nuxtjs/composition-api/module",
-    "@nuxtjs/svg",
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ["@nuxtjs/composition-api/module", "@nuxtjs/svg"],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
+   ** Nuxt.js modules
+   */
+  modules: [],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
+     ** You can extend webpack config here
+     */
     postcss: {
       postcssOptions: {
         plugins: {
@@ -65,7 +59,6 @@ export default {
         },
       },
     },
-    extend (config, ctx) {
-    }
-  }
-}
+    extend(config, ctx) {},
+  },
+};
