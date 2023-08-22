4c098df84ef78553ba711acc67e7ab7d3ae064d4
Donya <thisis2nya@gmail.com>
2023-08-22 14:50:10 +0330

    [UI] fix bookmarks list overflow

--- components/header/AppBookmarks.vue
+++ components/header/AppBookmarks.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="absolute top-6 w-64 bg-zinc-50 shadow-xl rounded-lg hidden peer-hover:flex hover:flex transition-all flex-col items-center justify-start z-40"
+    class="absolute top-6 w-64 max-h-[80vh] bg-zinc-50 shadow-xl rounded-lg hidden peer-hover:flex hover:flex transition-all flex-col items-center justify-start z-40 overflow-y-auto"
   >
     <bookmark-item
       v-for="(bookmark, i) in bookmarks"
@@ -27,3 +27,10 @@ const bookmarks = computed(() => {
   return store.state.bookmarks;
 });
 </script>
+
+<style scoped>
+::-webkit-scrollbar-thumb {
+  margin-right: 2px;
+  background-color: #fbba8960;
+}
+</style>
